2147273f9a190a57fda3a54487a6f9f960ca24a0
riki <iphoneseisaku@gmail.com>
2017-03-04 14:43:15 +0900

    TestB_1_2

--- app/src/main/java/com/example/riki/reverttest/MainActivity.java
+++ app/src/main/java/com/example/riki/reverttest/MainActivity.java
@@ -10,8 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Master1;
+        TestB1;
         Master5;
         Master6;
+        TestB2;
         Master7;
         Master8;
     }
